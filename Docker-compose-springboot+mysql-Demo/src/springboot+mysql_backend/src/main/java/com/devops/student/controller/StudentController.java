152194860a43877a385c87c79fd5f50d286ff593
Renilraphy906 <55274559+Renilraphy906@users.noreply.github.com>
2020-04-18 23:40:20 +0530

    Docker_demo : spring_boot+mysql : 1

---
+++ Docker-compose-springboot+mysql-Demo/src/springboot+mysql_backend/src/main/java/com/devops/student/controller/StudentController.java
@@ -0,0 +1,47 @@
+package com.devops.student.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devops.student.domain.Student;
+import com.devops.student.service.StudentService;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+
+	@Autowired
+	StudentService studentService;
+	
+	@GetMapping("/hello")
+	public String hello()
+	{
+		return "hello";
+		
+	}
+	
+	@GetMapping("/add/name={name}")
+	public Student newStudent(@PathVariable String name)
+	{
+		Student student = new Student();
+		student.setName(name);
+		
+		return studentService.insert(student);
+		
+	}
+	
+	@GetMapping("/find")
+	public List<Student> findStudent(){
+		return studentService.findAll();
+		
+	}
+	
+}
---
+++ Docker-compose-springboot+mysql-Demo/src/springboot+mysql_backend/src/main/java/com/devops/student/domain/Student.java
@@ -0,0 +1,43 @@
+package com.devops.student.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="student")
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	private String name;
+	
+	public Student() {
+		// TODO Auto-generated constructor stub
+	}
+	public Student(int id,String name) {
+		// TODO Auto-generated constructor stub
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+}
